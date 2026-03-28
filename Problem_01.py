bed2767195d4da718ed68bacc26b921555d8796d
divyansh24-ctrl <divyansh.sharma@avantika.edu.in>
2026-03-28 12:08:00 +0000

    complete the code

--- Problem_01.py
+++ Problem_01.py
@@ -3,11 +3,6 @@ def calculate_fine(book_title, days_overdue, daily_rate=5.00, max_fine=150.0):
     if fine > max_fine:
         fine = max_fine
     return fine
-def calculate_fine(book_title, days_overdue, daily_rate=5.00, max_fine=150.0):
-    fine = days_overdue * daily_rate
-    if fine > max_fine:
-        fine = max_fine
-    return fine
 
 
 book_title = input()
