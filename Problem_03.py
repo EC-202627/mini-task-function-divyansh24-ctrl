4289f16ab8b2f17705c17120bbc5d89dcbdd3481
divyansh24-ctrl <divyansh.sharma@avantika.edu.in>
2026-03-28 14:07:51 +0000

    add problem 4

--- Problem_03.py
+++ Problem_03.py
@@ -2,6 +2,7 @@ def calculate_fine(book_title, days_overdue, daily_rate=5.00, max_fine=150.0):
     fine = days_overdue * daily_rate
     if fine > max_fine:
         fine = max_fine
+        
     return fine
 
 
